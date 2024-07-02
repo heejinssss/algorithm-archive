1fdcfb95ac6a8b4dc53fb32ab291d7f1ab6b05d9
heejinssss <heejin.dev33@gmail.com>
2024-07-02 16:31:27 +0900

    Feat: Solved Loop Problems

---
+++ data_structure/baekjoon/Bronze_2_시험감독.py
@@ -0,0 +1,24 @@
+def cal(newList, ability):
+  supervisor = 0
+  for n in newList:
+    a = n // ability
+    if (n % ability != 0):
+      supervisor += a + 1
+    else:
+      supervisor += a
+
+  return supervisor
+
+n = int(input())
+students = list(map(int, input().split()))
+main, sub = map(int, input().split())
+
+stack = []
+cnt = 0
+for student in students:
+  cnt += 1
+  if (student-main) > 0:
+    stack.append(student-main)
+b = cal(stack, sub)
+
+print(cnt + b)
