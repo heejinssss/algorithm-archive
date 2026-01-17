c06a12e00f82113b2421d31808d140caec2745ae
Heejin <122508547+heejinssss@users.noreply.github.com>
2026-01-18 00:41:32 +0900

    Feat: Solved Greedy Problems

---
+++ data_structure/programmers/Lv0_수열과구간쿼리4.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+        int s = 0;
+        int e = 0;
+        int k = 0;
+        
+        for (int[] query : queries) {
+            s = query[0];
+            e = query[1];
+            k = query[2];
+
+            for (int i = s; i <= e; i++) {
+                if (i % k == 0) {
+                    arr[i] += 1;
+                }
+            }
+        }
+
+        answer = arr;
+
+        return answer;
+    }
+}
