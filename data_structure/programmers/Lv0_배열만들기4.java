c9ec47058b6b8cb04d8cdf31d1e350d6914c87e5
Heejin <122508547+heejinssss@users.noreply.github.com>
2026-01-20 22:01:13 +0900

    Feat: Solved Greedy Problems

---
+++ data_structure/programmers/Lv0_배열만들기4.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] arr) {
+
+        ArrayList<Integer> stk = new ArrayList<>();
+        int i = 0;
+
+        while (i < arr.length) {
+            if (stk.size() == 0) {
+                stk.add(arr[i]);
+                i += 1;
+            } else if (stk.size() != 0 && stk.get(stk.size()-1) < arr[i]) {
+                stk.add(arr[i]);
+                i += 1;
+            } else if (stk.size() != 0 && stk.get(stk.size()-1) >= arr[i]) {
+                stk.remove(stk.size()-1);
+            }
+        }
+    
+        int[] answer = stk.stream().mapToInt(Integer::intValue).toArray();
+
+        return answer;
+    }
+}
