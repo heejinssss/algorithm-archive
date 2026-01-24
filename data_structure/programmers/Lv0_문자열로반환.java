8901c9db4678c594137036ef3d86dcf751f67275
Heejin <122508547+heejinssss@users.noreply.github.com>
2026-01-24 20:40:02 +0900

    Feat: Solved Greedy Problems

---
+++ data_structure/programmers/Lv0_문자열로반환.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        
+        // answer = String.valueOf(n);
+        // answer = Integer.toString(n);
+        answer = n + "";
+
+        return answer;
+    }
+}
