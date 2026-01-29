5ebcf0c58f8db822def50b452c80233e04d42500
Heejin <122508547+heejinssss@users.noreply.github.com>
2026-01-30 00:24:31 +0900

    Feat: Solved Greedy Problems

---
+++ data_structure/programmers/Lv0_할일목록.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+
+        List<String> answer = new ArrayList<>();
+
+        for (int i = 0; i < finished.length; i++) {
+            if (!finished[i]) {
+                answer.add(todo_list[i]);
+            }
+        }
+
+        return answer.stream().toArray(String[]::new);
+    }
+}
