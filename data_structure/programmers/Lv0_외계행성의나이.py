632c67649d093598c0baa2a33bbf12e1194554c2
Heejin <122508547+heejinssss@users.noreply.github.com>
2026-03-19 19:48:34 +0900

    Feat: Solved Greedy Problems

---
+++ data_structure/programmers/Lv0_외계행성의나이.py
@@ -0,0 +1,17 @@
+def solution(age):
+    answer = ''
+    dic = {'0': 'a',
+           '1': 'b',
+           '2': 'c',
+           '3': 'd',
+           '4': 'e',
+           '5': 'f',
+           '6': 'g',
+           '7': 'h',
+           '8': 'i',
+           '9': 'j' }
+
+    for num in str(age):
+        answer += dic[num]
+
+    return answer
