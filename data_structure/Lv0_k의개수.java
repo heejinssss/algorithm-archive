e9fbf818601f82cca55ba5a6c45e9ff579022662
Heejin <122508547+heejinssss@users.noreply.github.com>
2026-05-05 00:47:27 +0900

    Feat: Solved Greedy Problems

--- data_structure/Lv0_k의개수.java
+++ data_structure/Lv0_k의개수.java
@@ -6,6 +6,7 @@ class Solution {
         for (int s = i; s < j+1; s++) {
             String s_str = s+"";
             String k_str = k+"";
+
             if (s_str.contains(k_str)) {
                 for (int si = 0; si < s_str.length(); si++) {
                     answer = s_str.substring(si, si+1).contains(k_str) ? answer + 1 : answer;
