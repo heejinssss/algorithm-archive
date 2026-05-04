778c0b4f964dc68257bec853a64a4b42ee1c8c85
Heejin <122508547+heejinssss@users.noreply.github.com>
2026-05-05 00:46:07 +0900

    Feat: Solved Greedy Problems

--- data_structure/programmers/Lv0_특수문자출력하기.java
+++ data_structure/programmers/Lv0_특수문자출력하기.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         System.out.print("!@#$%^&*(\\\'\"<>?:;");
-        /*
-        출력 결과 : !@#$%^&*(\'"<>?:;
-        */
+        /* 출력 결과 : !@#$%^&*(\'"<>?:; */
     }
 }
