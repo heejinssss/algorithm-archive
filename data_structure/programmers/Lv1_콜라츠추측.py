967d6b66030f883ba73db9b0fc53c79db5ec2cd8
heejinssss <heejin.dev33@gmail.com>
2026-05-21 23:08:50 +0900

    Feat: Solved Greedy Problems

---
+++ data_structure/programmers/Lv1_콜라츠추측.py
@@ -0,0 +1,10 @@
+def solution(num):
+    cnt = 0
+
+    while (cnt <= 500):
+        if num == 1:
+            return cnt
+        num = num * 3 + 1 if num % 2 else num // 2
+        cnt += 1
+
+    return -1
