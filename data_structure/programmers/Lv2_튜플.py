d7edf100948e4ea13901dcf528cffde6dd102e52
Heejin <122508547+heejinssss@users.noreply.github.com>
2026-07-07 00:08:33 +0900

    Feat: Solved Greedy Problems

--- data_structure/programmers/Lv2_튜플.py
+++ data_structure/programmers/Lv2_튜플.py
@@ -1,11 +1,31 @@
+"""
+# 교집합
+print(set1 & set2)
+print(set1.intersection(set2))
+
+# 합집합
+print(set1 | set2)
+print(set1.union(set2))
+
+# 차집합
+print(set1 - set2)
+print(set1.difference(set2))
+"""
+
 def solution(s):
     answer = []
 
-    arr = s[2:len(s)-2].split("},{")
-    arr = sorted(arr, key=lambda x : len(x))
+    tmp = sorted(s[2:len(s)-2].split("},{"), key=lambda x : len(x))
+    arr = []
 
-    for a in arr:
-        aList = a.split(",")
-        print(aList)
+    for t in tmp:
+        lst = t.split(",")
+        arr.append(lst)
+
+    answer.append(int(arr[0][0]))
+
+    for i in range(len(arr)-1):
+        set1, set2 = set(arr[i]), set(arr[i+1])
+        answer.append(int(list(set2-set1)[0]))
 
     return answer
