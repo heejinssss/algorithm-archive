2cdaecde8eaea1505ca68618160941c5577717db
Heejin <122508547+heejinssss@users.noreply.github.com>
2026-07-14 20:26:42 +0900

    Feat: Solved Greedy Problems

--- data_structure/programmers/Lv1_[1차]다트게임.py
+++ data_structure/programmers/Lv1_[1차]다트게임.py
@@ -1,3 +1,25 @@
+from collections import deque
+
 def solution(dartResult):
-    answer = 0
-    return answer
+
+    score = { "S": 1, "D": 2, "T": 3 }
+    bonus = { "*": 2, "#": -1 }
+    num = deque()
+    tmp = ""
+
+    for i in range(len(dartResult)):
+        if dartResult[i].isdigit():
+            tmp += dartResult[i]
+        else:
+            if dartResult[i] in score.keys():
+                cur = int(tmp)
+                tmp = ""
+                cur **= score[dartResult[i]]
+            if dartResult[i] in bonus.keys():
+                cur = num.pop()
+                cur *= bonus[dartResult[i]]
+            if 0 <= i+3 < len(dartResult) and dartResult[i+3] == "*":
+                cur *= 2
+            num.append(cur)
+
+    return sum(num)
