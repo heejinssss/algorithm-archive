417b428a169a847971a2335f7880a626b9d959e2
Heejin <122508547+heejinssss@users.noreply.github.com>
2026-08-07 22:51:28 +0900

    Feat: Solved Greedy Problems

---
+++ data_structure/programmers/Lv0_대문자와소문자.py
@@ -0,0 +1,15 @@
+def solution(my_string):
+
+    """
+    answer = ""
+    
+    for string in my_string:
+        if string.isupper():
+            answer += string.lower()
+        else:
+            answer += string.upper()
+
+    return answer
+    """
+
+    return my_string.swapcase()
