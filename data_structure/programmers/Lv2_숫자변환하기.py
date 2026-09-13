d2206a90eb784029045f1e4dd595d6a057724646
Heejin <122508547+heejinssss@users.noreply.github.com>
2026-09-13 23:47:02 +0900

    Feat: Solved Greedy Problems

--- data_structure/programmers/Lv2_숫자변환하기.py
+++ data_structure/programmers/Lv2_숫자변환하기.py
@@ -1,8 +1,21 @@
 def solution(x, y, n):
     answer = 0
-    """
-    x += n
-    x *= 2
-    x *= 3
-    """
+
+    case = [[y, 0]]
+    result = []
+
+    while case:
+        cur, cnt = case.pop(0)
+        if cur <= x:
+            if cur == x:
+                return cnt
+            else:
+                return -1
+        if cur >= x and cur%2 == 0:
+            case.append([cur//2, cnt+1])
+        if cur >= x and cur%3 == 0:
+            case.append([cur//3, cnt+1])
+        if cur >= x:
+            case.append([cur-n, cnt+1])
+
     return answer
