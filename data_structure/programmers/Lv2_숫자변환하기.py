f2b7591a9984da1223005cac220f601d79249e70
Heejin <122508547+heejinssss@users.noreply.github.com>
2026-09-14 22:59:21 +0900

    Feat: Solved Greedy Problems

--- data_structure/programmers/Lv2_숫자변환하기.py
+++ data_structure/programmers/Lv2_숫자변환하기.py
@@ -1,21 +1,31 @@
 def solution(x, y, n):
-    answer = 0
-
+    visited = [0] * (y+1)
     case = [[y, 0]]
     result = []
 
     while case:
         cur, cnt = case.pop(0)
-        if cur <= x:
-            if cur == x:
-                return cnt
-            else:
-                return -1
-        if cur >= x and cur%2 == 0:
-            case.append([cur//2, cnt+1])
-        if cur >= x and cur%3 == 0:
-            case.append([cur//3, cnt+1])
-        if cur >= x:
-            case.append([cur-n, cnt+1])
 
-    return answer
+        if cur == x:
+            result.append(cnt)
+            continue
+
+        if cur < 1:
+            continue
+
+        if cur%2 == 0 and visited[cur//2] == 0 and cur >= x:
+            case.append([cur//2, cnt+1])
+            visited[cur//2] = 1
+
+        if cur%3 == 0 and visited[cur//3] == 0 and cur >= x:
+            case.append([cur//3, cnt+1])
+            visited[cur//3] = 1
+
+        if visited[cur-n] == 0 and cur >= x:
+            case.append([cur-n, cnt+1])
+            visited[cur-n] = 1
+
+    if not len(result):
+        return -1
+
+    return min(result)
