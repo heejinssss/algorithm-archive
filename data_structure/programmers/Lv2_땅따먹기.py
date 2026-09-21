1f6705581c041e82b3ea5a7db489468ecb666df3
Heejin <122508547+heejinssss@users.noreply.github.com>
2026-09-21 23:34:20 +0900

    Feat: Solved Greedy Problems

--- data_structure/programmers/Lv2_땅따먹기.py
+++ data_structure/programmers/Lv2_땅따먹기.py
@@ -1,6 +1,13 @@
-# 풀이중
-
 def solution(land):
-    answer = 0
+    cand = []
 
-    return answer
+    for i in range(4):
+        prev_index = i
+        value = land[0][i]
+        for j in range(1, len(land)):
+            land[j][prev_index] = 0
+            value += max(land[j])
+            prev_index = land[j].index(max(land[j]))
+        cand.append(value)
+
+    return max(cand)
